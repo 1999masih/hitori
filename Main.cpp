2dab6b4cfe5c4cd8c4dca2c66a443e095eb7597a
amirhossein <amir77mafi@gmail.com>
2019-11-30 19:21:15 +0330

    the end of the fucking successor

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 #include <vector>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-bool is_goal(int** table, int row, int col)
+bool is_goal(int** table, bool** state, int row, int col)
 {
     for (int i = 0; i < row; i++)
     {
@@ -13,7 +14,7 @@ bool is_goal(int** table, int row, int col)
         {
             for (int k = j + 1; k < col; k++)
             {
-                if (table[i][j] == table[i][k])
+                if (table[i][j] == table[i][k] && state[i][j] && state[i][k])
                 {
                     return false;
                 }
@@ -27,7 +28,7 @@ bool is_goal(int** table, int row, int col)
         {
             for (int k = j + 1; k < row; k++)
             {
-                if (table[j][i] == table[k][j])
+                if (table[j][i] == table[k][j] && state[j][i] && state[k][j])
                 {
                     return false;
                 }
@@ -38,20 +39,131 @@ bool is_goal(int** table, int row, int col)
     return true;
 }
 
-vector<int**> successor(int** table, int row, int col)
+bool** CopyState(bool** state, int row, int col)
 {
+    bool** new_state = new bool*[row];
+    for (int i = 0; i < row; i++)
+    {
+        new_state[i] = new bool[col];
+        for (int j = 0; j < col; j++)
+        {
+            new_state[i][j] = state[i][j];
+        }
+    }
+    return new_state;
+}
 
+void DeleteState(bool** state, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        delete[] state[i];
+    }
+    delete[] state;
+}
+
+vector<bool**> successor(int** table, bool** state, int row, int col)
+{
+    vector<bool**> result;
+    for (int i = 0; i < row; i++)
+    {
+        // vector<int> repeateds;
+        for (int j = 0; j < col; j++)
+        {
+            
+            // if (!(find(repeateds.begin(), repeateds.end(), table[i][j]) != repeateds.end()))
+            // {
+            //     bool flag = false;
+                bool** tmpState = CopyState(state, row, col);
+                bool twoWhites = false;
+                // if (!(!table[i][j-1] && !table[i][j+1] && !table[i-1][j] && !table[i+1][j]))
+
+                for (int k = 0; k < col; k++)
+                {
+                    if ( (i-1 >= 0 && !tmpState[i-1][k]) || 
+                         (i+1 < row && !tmpState[i+1][k]) ||
+                         (k-1 >= 0 && !tmpState[i][k-1]) ||
+                         (k+1 < col && !tmpState[i][k+1]) ||
+                         
+                         (
+                            ( (i+1 < row && k+1 < col && !tmpState[i+1][k+1]) && (i+2 < row && !tmpState[i+2][k]) && (i+1 < row && k-1 >= 0 && !tmpState[i+1][k-1]) ) ||
+                            ( (i+1 < row && k+1 < col && !tmpState[i+1][k+1]) && (k+2 < col && !tmpState[i][k+2]) && (i-1 >= 0 && k+1 < col && !tmpState[i-1][k+1]) ) ||
+                            ( (i-1 >= 0 && k-1 >= 0 && !tmpState[i-1][k-1]) && (i-2 >= 0 && !tmpState[i-2][k]) && (i-1 >= 0 && k+1 < col && !tmpState[i-1][k+1]) ) ||
+                            ( (i-1 >= 0 && k-1 >= 0 && !tmpState[i-1][k-1]) && (k-2 >= 0 && !tmpState[i][k-2]) && (i+1 < row && k-1 >= 0 && !tmpState[i+1][k-1]) )
+                         )
+                         
+                         )
+                    {
+                        twoWhites = true;
+                        break;
+                    }
+
+                    if (table[i][j] == table[i][k])
+                    {
+                        tmpState[i][k] = false;
+                    }
+                }
+
+                if (!twoWhites)
+                {
+                    for (int k = 0; k < row; k++)
+                    {
+                        if ( (k-1 >= 0 && !tmpState[k-1][j]) || 
+                            (k+1 < col && !tmpState[k+1][j]) ||
+                            (j-1 >= 0 && !tmpState[k][j-1]) ||
+                            (j+1 < row && !tmpState[k][j+1]))
+                        {
+                            twoWhites = true;
+                            break;
+                        }
+
+                        if (table[i][j] == table[k][j])
+                        {
+                            tmpState[k][j] = false;
+                        }
+                    }
+                }
+
+                if (!twoWhites)
+                {
+                    result.push_back(tmpState);
+                }
+                else
+                {
+                    DeleteState(tmpState, row, col);
+                }
+                
+            //     if (flag)
+            //     {
+            //         bool** tmpState = CopyState(state, row, col);
+            //         tmpState[i][j] = false;
+            //         result.push_back(tmpState);
+            //         repeateds.push_back(table[i][j]);
+            //     }
+            // }
+        }
+    }
 }
 
 void greedy(int** table, int row, int col)
 {
-    int** current_state = table;
-    vector<int**> current_state_childs;
+    bool** current_state = new bool*[row];
+    for (int i = 0; i < row; i++)
+    {
+        current_state[i] = new bool[col];
+        for (int j = 0; j < col; j++)
+        {
+            current_state[i][j] = true;
+        }
+    }
 
-    while (!is_goal(current_state, row, col))
+    // int** current_state = table;
+    vector<bool**> current_state_childs;
+
+    while (!is_goal(table, current_state, row, col))
     {
 
-        current_state_childs = successor(current_state, row, col);
+        current_state_childs = successor(table, current_state, row, col);
 
         current_state = heuristic(current_state_childs, row, col);
 
